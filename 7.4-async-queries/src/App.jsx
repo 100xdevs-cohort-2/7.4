b707c52451083b14fadc48b0e2df4ba744f4baa9
gulshan-dubey <gulshand14@gmail.com>
2024-01-20 00:01:33 +0530

    fixed! not reflecting network value in async-queries

--- 7.4-async-queries/src/App.jsx
+++ 7.4-async-queries/src/App.jsx
@@ -1,40 +1,48 @@
-
-import './App.css'
-import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
-import { notifications, totalNotificationSelector } from './atoms'
-import { useEffect } from 'react'
-import axios from 'axios'
+import "./App.css";
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+import { notifications, totalNotificationSelector } from "./atoms";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <RecoilRoot>
-    <MainApp />
-  </RecoilRoot>
+  return (
+    <RecoilRoot>
+      <MainApp />
+    </RecoilRoot>
+  );
 }
 
 function MainApp() {
-  const [networkCount, setNetworkCount] = useRecoilState(notifications)
+  const [networkCount, setNetworkCount] = useRecoilState(notifications);
   const totalNotificationCount = useRecoilValue(totalNotificationSelector);
 
   useEffect(() => {
     // fetch
-    axios.get("https://sum-server.100xdevs.com/notifications")
-      .then(res => {
-        setNetworkCount(res.data)
-      })
-  }, [])
+    axios.get("https://sum-server.100xdevs.com/notifications").then((res) => {
+      setNetworkCount(res.data);
+    });
+  }, []);
 
   return (
     <>
       <button>Home</button>
-      
-      <button>My network ({networkCount.networks >= 100 ? "99+" : networkCount.networks})</button>
+
+      <button>
+        My network ({networkCount.network >= 100 ? "99+" : networkCount.network}
+        )
+      </button>
       <button>Jobs {networkCount.jobs}</button>
       <button>Messaging ({networkCount.messaging})</button>
       <button>Notifications ({networkCount.notifications})</button>
 
       <button>Me ({totalNotificationCount})</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
