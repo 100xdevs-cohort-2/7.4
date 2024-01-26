dfc7b30cb7e29cc5f888ae82d45b497ffd11028a
gulshan-dubey <gulshand14@gmail.com>
2024-01-26 15:02:59 +0530

    Learned Selector Family

--- selector-family/src/App.jsx
+++ selector-family/src/App.jsx
@@ -1,25 +1,25 @@
-
-import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
-import { todosAtomFamily } from './atoms';
+import "./App.css";
+import { RecoilRoot, useRecoilState } from "recoil";
+import { todosAtomFamily } from "./atoms";
 
 function App() {
-  return <RecoilRoot>
-    <Todo id={1}/>
-    <Todo id={2} />
-  </RecoilRoot>
+  return (
+    <RecoilRoot>
+      <Todo id={1} />
+      <Todo id={2} />
+    </RecoilRoot>
+  );
 }
 
-function Todo({id}) {
-   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+function Todo({ id }) {
+  const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
 
   return (
     <>
-      {todo.title}
-      {todo.description}
+      {todo.title} - {todo.description}
       <br />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
